ac38d70a9eb1b7d319a84936f9b4c5abe8fe463c
Eric W. Brown <feneric@gmail.com>
2019-12-31 22:41:07 -0500

    Improved update handling.

--- fortune.py
+++ fortune.py
@@ -1,7 +1,7 @@
 import logging
 from random import choice, random
 from subprocess import run, PIPE
-from pwnagotchi.plugins import Plugin
+import pwnagotchi.plugins as plugins
 
 '''
 This needs the fortune program installed along with a database of fortunes.
@@ -18,35 +18,35 @@ configuration file.
 '''
 
 
-class Fortune(Plugin):
+class Fortune(plugins.Plugin):
     __author__ = '@Feneric'
-    __version__ = '0.1.0'
+    __version__ = '0.2.0'
     __license__ = 'GPL3'
     __description__ = 'Alleviate a little boredom by quoting random fortunes.'
 
-    def __init__(self):
-        # The fortune program only supports a dozen languages at the moment.
-        self.langs = ('bg', 'br', 'cs', 'de', 'eo', 'es', 'fr', 'ga', 'it', 'pl', 'ru', 'zh')
-        self.fortune = ""
+    # The fortune program only supports a dozen languages at the moment.
+    __langs = ('bg', 'br', 'cs', 'de', 'eo', 'es', 'fr', 'ga', 'it', 'pl', 'ru', 'zh')
 
     def set_fortune(self, agent):
         display = agent.view()
         config = agent.config()
         try:
             lang = config['main']['lang'][:2]
-            if lang not in self.langs:
-                lang = ''
         except KeyError:
+            lang = 'en'
+        if lang not in Fortune.__langs:
             lang = ''
         try:
             alt_cmd = config['plugins']['fortune']['alt_cmd']
         except KeyError:
             alt_cmd = ''
-        cmd = alt_cmd.split() or ['/usr/games/fortune', '-s', '-n', '120', lang]
+        fortune_length = 120
+        cmd = alt_cmd.split() or ['/usr/games/fortune', '-s', '-n', str(fortune_length), lang]
         proc = run(cmd, stdout=PIPE)
-        self.fortune = proc.stdout.decode('utf-8')
-        logging.info("[fortune] {}".format(self.fortune))
-        display.update(force=True)
+        fortune = proc.stdout.decode('utf-8')
+        logging.info("[fortune] {}".format(fortune))
+        face = choice(['(☉‗☉ )', '( ☉‗☉)'])
+        display.update(force=True, new_data={'face': face, 'status': fortune})
 
     def on_loaded(self):
         logging.info("Fortune plugin loaded")
@@ -56,15 +56,9 @@ class Fortune(Plugin):
             self.set_fortune(agent)
 
     def on_lonely(self, agent):
-        if random() > .8:
+        if random() > .7:
             self.set_fortune(agent)
 
     def on_sad(self, agent):
-        if random() > .9:
+        if random() > .8:
             self.set_fortune(agent)
-
-    def on_ui_update(self, ui):
-        if self.fortune:
-            ui.set('face', choice(['(☉‗☉ )', '( ☉‗☉)']))
-            ui.set('status', self.fortune)
-            self.fortune = ""
